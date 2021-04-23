392bb2243ad35e9a40035cfa00be72a3309dd1b4
Hiroyuki Chihara <77265300+hiroyuki-chihara@users.noreply.github.com>
2021-04-23 17:43:46 +0900

    Add files via upload

--- jhu.m
+++ jhu.m
@@ -23,44 +23,44 @@ l3=datestr(d1,'yyyy-mm-dd');
 
 % Japan
 PJPN=126;
-AJPN=transpose(A.data(155,3:qa))/PJPN;
-BJPN=transpose(B.data(155,3:qa))/PJPN;
+AJPN=transpose(A.data(156,3:qa))/PJPN;
+BJPN=transpose(B.data(156,3:qa))/PJPN;
 % South Korea
 PKOR=51.3;
-AKOR=transpose(A.data(159,3:qa))/PKOR;
-BKOR=transpose(B.data(159,3:qa))/PKOR;
+AKOR=transpose(A.data(160,3:qa))/PKOR;
+BKOR=transpose(B.data(160,3:qa))/PKOR;
 % Philippines
 PPHI=108;
-APHI=transpose(A.data(209,3:qa))/PPHI;
-BPHI=transpose(B.data(209,3:qa))/PPHI;
+APHI=transpose(A.data(210,3:qa))/PPHI;
+BPHI=transpose(B.data(210,3:qa))/PPHI;
 % Malaysia
 PMYS=32;
-AMYS=transpose(A.data(175,3:qa))/PMYS;
-BMYS=transpose(B.data(175,3:qa))/PMYS;
+AMYS=transpose(A.data(176,3:qa))/PMYS;
+BMYS=transpose(B.data(176,3:qa))/PMYS;
 % Indonesia
 PIDN=271;
-AIDN=transpose(A.data(148,3:qa))/PIDN;
-BIDN=transpose(B.data(148,3:qa))/PIDN;
+AIDN=transpose(A.data(149,3:qa))/PIDN;
+BIDN=transpose(B.data(149,3:qa))/PIDN;
 % India
 PIND=1380;
-AIND=transpose(A.data(147,3:qa))/PIND;
-BIND=transpose(B.data(147,3:qa))/PIND;
+AIND=transpose(A.data(148,3:qa))/PIND;
+BIND=transpose(B.data(148,3:qa))/PIND;
 % Nepal
 PNPL=29.5;
-ANPL=transpose(A.data(191,3:qa))/PNPL;
-BNPL=transpose(B.data(191,3:qa))/PNPL;
+ANPL=transpose(A.data(192,3:qa))/PNPL;
+BNPL=transpose(B.data(192,3:qa))/PNPL;
 % Pakistan
 PPAK=221;
-APAK=transpose(A.data(204,3:qa))/PPAK;
-BPAK=transpose(B.data(204,3:qa))/PPAK;
+APAK=transpose(A.data(205,3:qa))/PPAK;
+BPAK=transpose(B.data(205,3:qa))/PPAK;
 % Bangladesh
 PBGD=165;
 ABGD=transpose(A.data(21,3:qa))/PBGD;
 BBGD=transpose(B.data(21,3:qa))/PBGD;
 % Sri Lanka
 PLKA=21.5;
-ALKA=transpose(A.data(235,3:qa))/PLKA;
-BLKA=transpose(B.data(235,3:qa))/PLKA;
+ALKA=transpose(A.data(236,3:qa))/PLKA;
+BLKA=transpose(B.data(236,3:qa))/PLKA;
 % Argentina 8 8
 PARG=45.2;
 AARG=transpose(A.data(7,3:qa))/PARG;
@@ -71,32 +71,32 @@ ABRA=transpose(A.data(31,3:qa))/PBRA;
 BBRA=transpose(B.data(31,3:qa))/PBRA;
 % Colombia 93 78
 PCOL=50.9;
-ACOL=transpose(A.data(92,3:qa))/PCOL;
-BCOL=transpose(B.data(92,3:qa))/PCOL;
+ACOL=transpose(A.data(93,3:qa))/PCOL;
+BCOL=transpose(B.data(93,3:qa))/PCOL;
 % France 131 116
 PFRA=65.3;
-AFRA=transpose(A.data(130,3:qa))/PFRA;
-BFRA=transpose(B.data(130,3:qa))/PFRA;
+AFRA=transpose(A.data(131,3:qa))/PFRA;
+BFRA=transpose(B.data(131,3:qa))/PFRA;
 % Italia 154 139
 PITA=60.5;
-AITA=transpose(A.data(153,3:qa))/PITA;
-BITA=transpose(B.data(153,3:qa))/PITA;
+AITA=transpose(A.data(154,3:qa))/PITA;
+BITA=transpose(B.data(154,3:qa))/PITA;
 % Mexico 183 168
 PMEX=129;
-AMEX=transpose(A.data(182,3:qa))/PMEX;
-BMEX=transpose(B.data(182,3:qa))/PMEX;
+AMEX=transpose(A.data(183,3:qa))/PMEX;
+BMEX=transpose(B.data(183,3:qa))/PMEX;
 % Spain 235 220
 PESP=46.8;
-AESP=transpose(A.data(234,3:qa))/PESP;
-BESP=transpose(B.data(234,3:qa))/PESP;
+AESP=transpose(A.data(235,3:qa))/PESP;
+BESP=transpose(B.data(235,3:qa))/PESP;
 % United Staes 251 236
 PUSA=331;
-AUSA=transpose(A.data(250,3:qa))/PUSA;
-BUSA=transpose(B.data(250,3:qa))/PUSA;
+AUSA=transpose(A.data(251,3:qa))/PUSA;
+BUSA=transpose(B.data(251,3:qa))/PUSA;
 % United Kingdom 266 251
 PGBR=67.9;
-AGBR=transpose(A.data(265,3:qa))/PGBR;
-BGBR=transpose(B.data(265,3:qa))/PGBR;
+AGBR=transpose(A.data(266,3:qa))/PGBR;
+BGBR=transpose(B.data(266,3:qa))/PGBR;
 
 newcolors = [0 0 0; 
              0 1 0; 
