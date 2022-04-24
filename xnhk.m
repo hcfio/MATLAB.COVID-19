95c3e5b04cbb073dbad2433b20ba74941d94ae4e
Hiroyuki Chihara <77265300+hiroyuki-chihara@users.noreply.github.com>
2022-04-24 23:59:30 +0900

    Add files via upload

--- xnhk.m
+++ xnhk.m
@@ -52,27 +52,57 @@ for j=1:D
     OSK3(j,1)=(OSK1(j+716,1)-OSK1(j+709,1))/7;
 end
 
+% Kyushu code 40 41 42 43 44 45 46
+PFUK=5.112176;
+PSAG=0.805721;
+PNAG=1.290992;
+PKUM=1.722474;
+POIT=1.110553;
+PMIY=1.057609;
+PKAG=1.571833;
+PKYU=PFUK+PSAG+PNAG+PKUM+POIT+PMIY+PKAG;
+ROWFUK=find(B(:)==40);
+ROWSAG=find(B(:)==41);
+ROWNAG=find(B(:)==42);
+ROWKUM=find(B(:)==43);
+ROWOIT=find(B(:)==44);
+ROWMIY=find(B(:)==45);
+ROWKAG=find(B(:)==46);
+FUK1=A.data(ROWFUK,2);
+SAG1=A.data(ROWSAG,2);
+NAG1=A.data(ROWNAG,2);
+KUM1=A.data(ROWKUM,2);
+OIT1=A.data(ROWOIT,2);
+MIY1=A.data(ROWMIY,2);
+KAG1=A.data(ROWKAG,2);
+KYU1=(FUK1+SAG1+NAG1+KUM1+OIT1+MIY1+KAG1)/PKYU;
+KYU3=zeros(D,1);
+for j=1:D
+    KYU3(j,1)=(KYU1(j+716,1)-KYU1(j+709,1))/7;
+end
+
+
 newcolors = [0 0 1; 
              1 131/255 0; 
              0 1 0; 
              1 0 0; 
+             0 191/255 1;
              138/255 43/255 226/255; 
              169/255 80/255 69/255;
              1 0 1; 
              220/255 220/255 220/255; 
-             1 191/255 17/255; 
-             0 191/255 1];
+             1 191/255 17/255];
 colororder(newcolors)         
 % c=hsv(6);
 % colororder(c);
 
 % plot
-plot([JPN3,TKY3,OKNW3,OSK3],'LineWidth',2)
+plot([JPN3,TKY3,OKNW3,OSK3,KYU3],'LineWidth',2)
 title('COVID-19: 7-day average of daily new cases per 1M','data sourced by NHK (Japanese Public TV)')
 xlabel('date');
 ylabel('cases/1M');
 xticks([1 32 60 91 D])
 xticklabels({[l0],[l1],[l2],[l3],[ll]})
-legend('Japan','Tokyo','Okinawa','Osaka','Location','northwest');
+legend('Japan','Tokyo','Okinawa','Osaka','Kyushu','Location','northwest');
 set(gcf,'Position',[600,200,600,400]);
 saveas(gcf,'xnhk.png');
